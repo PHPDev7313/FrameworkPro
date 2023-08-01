96abada480ea00ca5676c1d4694a0a07db849e5b
PHPDev7313 <61154728+PHPDev7313@users.noreply.github.com>
2023-07-31 23:20:03 -0400

    trying to get it to work

--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,15 @@
 <?php
-declare(strict_types=1);
 
-use JDS\Kernel;
-use JDS\Http\Request;
-use JDS\Routing\Router;
+declare(strict_types=1);
 
 define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH . '/vendor/autoload.php';
+use JDSFramework\Kernel;
+use JDSFramework\Http\Request;
+use JDSFramework\Routing\Router;
+
+
 
 $request = Request::createFromGlobals();
 
